a444119de06aa11f0d3852d18e1fdd4ae8a690f9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2011-04-27 00:39:56 -0700

    Fixed DynamicBookmarkerStub.

--- DynamicBookmarkerStub.js
+++ DynamicBookmarkerStub.js
@@ -2,8 +2,7 @@
     var s = document.createElement('script');
     s.onload = function() {DynamicBookmarker.enter()}; // enter is a function defined in the loaded script
     s.type = 'text/javascript';
-    //s.src = 'reader.js?'+Date.now();
-    s.src = 'http://jfly.github.com/DynamicBookmarker/reader.js';
+    s.src = 'http://jfly.github.com/DynamicBookmarker/DynamicBookmarker.js';
     //TODO - detect failure to load script
     document.body.appendChild(s);
 })();
