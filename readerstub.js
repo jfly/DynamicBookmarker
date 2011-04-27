a9f6fd97e5f40965050dd471555e302ac83506de
Jeremy Fleischman <jeremyfleischman@gmail.com>
2011-04-26 18:44:02 -0700

    Updated readerstub to use github page instead of dropbox.

--- readerstub.js
+++ readerstub.js
@@ -3,7 +3,7 @@
     s.onload = function() {DynamicBookmarker.enter()}; // enter is a function defined in the loaded script
     s.type = 'text/javascript';
     //s.src = 'reader.js?'+Date.now();
-    s.src = 'http://dl.dropbox.com/u/1463845/unhosted/reader.js';
+    s.src = 'http://jfly.github.com/DynamicBookmarker/reader.js';
     //TODO - detect failure to load script
     document.body.appendChild(s);
 })();
